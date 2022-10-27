2c8fc1949014a7fdf2e652dee47dc4fb95905c25
Soledad Sasia <soledadsasia@gmail.com>
2022-10-27 18:19:08 -0300

    Crea hero-carousel y nabvar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { HeroCarouselComponent } from './componentes/hero-carousel/hero-carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroCarouselComponent
   ],
   imports: [
     BrowserModule,
