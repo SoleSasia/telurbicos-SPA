64dc524f431b13ea4c248633519cd6fb72be51eb
Soledad Sasia <soledadsasia@gmail.com>
2022-11-18 23:06:39 -0300

    Utiliza ngStyle para dinamizar carga de imagenes en hero-carousel

--- src/app/componentes/hero-carousel/hero-carousel.component.ts
+++ src/app/componentes/hero-carousel/hero-carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { url } from 'inspector';
 
 @Component({
   selector: 'app-hero-carousel',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-carousel.component.css']
 })
 export class HeroCarouselComponent implements OnInit {
+
+  slides: string[] = ["./assets/bg-telurbicos.jpg","./assets/bg-homenajes.jpg", "./assets/bg-otros.jpg"];
+  titleTelurbicos:string = "Murales telúrbicos";
+  textTelurbicos:string = "Aquí podrás encontar todo nuestro trabajo telúrbico.";
+  bgImage:string = "./assets/bg-telurbicos.jpg";
 
   constructor() { }
 
