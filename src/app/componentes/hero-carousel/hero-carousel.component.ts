298da21f17c036881ad8bbe1594505f9986d0dde
Soledad Sasia <soledadsasia@gmail.com>
2022-11-26 11:41:55 -0300

    Utiliza *ngFor para los sliders en hero-carousel.html

--- src/app/componentes/hero-carousel/hero-carousel.component.ts
+++ src/app/componentes/hero-carousel/hero-carousel.component.ts
@@ -1,21 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-import { url } from 'inspector';
+import { SlideHero } from 'src/app/models/slideHero';
 
 @Component({
   selector: 'app-hero-carousel',
   templateUrl: './hero-carousel.component.html',
   styleUrls: ['./hero-carousel.component.css']
 })
+
 export class HeroCarouselComponent implements OnInit {
 
-  slides: string[] = ["./assets/bg-telurbicos.jpg","./assets/bg-homenajes.jpg", "./assets/bg-otros.jpg"];
+  //bg una imagen (prueba)
   titleTelurbicos:string = "Murales telúrbicos";
   textTelurbicos:string = "Aquí podrás encontar todo nuestro trabajo telúrbico.";
   bgImage:string = "./assets/bg-telurbicos.jpg";
+
+  //bg carousel arreglo de objetos
+  
+  slidesHero: SlideHero[] = [
+    {
+      id: 1,
+      img: "./assets/bg-telurbicos.jpg",
+      title: "Murales telúrbicos",
+      description: "Aquí podrás encontar todo nuestro trabajo telúrbico.",
+      link: ""
+    },
+    {
+      id: 2,
+      img: "./assets/bg-homenajes.jpg",
+      title: "Homenajes",
+      description: "Aquí podrás acceder a los murales donde hemos homenajeado a alguien.",
+      link: ""
+    },
+    {
+      id: 3,
+      img: "./assets/bg-otros.jpg",
+      title: "Otros murales",
+      description: "Y aquí podrás encontrar murales sobre temáticas diversas.",
+      link: ""
+    }
+  ];
+
+
+    
+ // "./assets/bg-homenajes.jpg", "./assets/bg-otros.jpg"];
+
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
+
+
+
