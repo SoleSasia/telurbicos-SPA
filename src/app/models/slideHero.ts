11981b5e210e134f47da6bc9bf7dfe63f1fcec57
Soledad Sasia <soledadsasia@gmail.com>
2022-11-26 11:39:59 -0300

    Crea folder models
    Crea modelo SlideHero

---
+++ src/app/models/slideHero.ts
@@ -0,0 +1,15 @@
+export class SlideHero {
+    id: number;
+    img: string;
+    title: string;
+    description: string;
+    link?: string;
+
+    constructor (id: number, img: string, title: string, description: string, link: string) {
+        this.id = id;
+        this.img = img;
+        this.title = title;
+        this.description = description;
+        this.link = link;
+    }
+}
