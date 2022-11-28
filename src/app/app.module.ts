ebdd8d52911c8d7b20f36404e9b405ed7c0e9503
Soledad Sasia <soledadsasia@gmail.com>
2022-11-28 16:43:34 -0300

    Crea componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { HeroCarouselComponent } from './componentes/hero-carousel/hero-carousel.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { TelurbicosComponent } from './componentes/telurbicos/telurbicos.component';
+import { HomeComponent } from './componentes/home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TelurbicosComponent } from './componentes/telurbicos/telurbicos.compone
     NavbarComponent,
     HeroCarouselComponent,
     FooterComponent,
-    TelurbicosComponent
+    TelurbicosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
