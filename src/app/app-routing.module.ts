85d1e1c3b9c1501109d325d1372ae174eaddf74c
Soledad Sasia <soledadsasia@gmail.com>
2022-12-02 14:41:30 -0300

    Implementa homenajes y configura routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 //importo componentes
 import { TelurbicosComponent } from './componentes/telurbicos/telurbicos.component';
 import { HomeComponent } from './componentes/home/home.component';
+import { HomenajesComponent } from './componentes/homenajes/homenajes.component';
 
 //defino rutas
 const routes: Routes = [
   {path: 'm-telurbicos', component: TelurbicosComponent},
+  {path: 'm-homenajes', component: HomenajesComponent},
+
   {path: '', component: HomeComponent},
 ];
 
